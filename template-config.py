12393d090a1f7eb1cd0478bfd86fe9de3d52560f
Alberth Matos <alberth@matos.cc>
2020-11-24 11:28:50 -0500

    added template config.py

---
+++ template-config.py
@@ -0,0 +1,12 @@
+# ELITE DANGEROUS LOG FILES
+# change to the location of your Elite Dangerous log files
+logLocation = ""
+
+# HUE BRIDGE IP ADDRESS
+# IP address of your Philips Hue Bridge
+# (see https://developers.meethue.com/develop/get-started-2/ step 2 for help)
+hueIP = ""
+
+# HUE LIGHT OR GROUP TO CONTROL
+# Name of Hue light or group
+hueLight = ''
