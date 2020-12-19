ca42a5ddde07a5738c2237b4d97a16dd40e9c73c
Alberth Matos <alberth@matos.cc>
2020-12-18 23:41:51 -0500

    WIP on GUI

--- gui.py
+++ gui.py
@@ -1,42 +1,42 @@
+import ipaddress
 import logging
 import logging.config
-import tkinter as tk
-import PySimpleGUI as sg
-import ipaddress
 import os
-import config
+
+import PySimpleGUI as sg
 
 from edhue import configure_logger, EDHue, initialize
 from hue_light import HueLightControl
 
 
 def GUImain():
-    def start(hueLight):
+    def start(hue_light):
         logger.info('Starting.')
-        hue = HueLightControl(hueLight)
-        EDHue(hueLight)
+        hue = HueLightControl(hue_light)
+        EDHue(hue_light)
         logger.info('ED Hue is active.  Awaiting events...')
         hue.light_on()
 
-    def cleanup(hueLight):
-        hue = HueLightControl(hueLight)
-        hue.light_off()
+    def cleanup(hue_light):
+        if hue_light != '':
+            hue = HueLightControl(hue_light)
+            hue.light_off()
         logger.info('See you in the Black, Commander!')
         logger.info('o7')
-        EDHue(hueLight).stop()
+        EDHue(hue_light).stop()
 
-    def configure_ui(hueIP='0.0.0.0', hueLight=''):
+    def configure_ui(hue_IP='0.0.0.0', hue_light=''):
         logger.debug('In Configure UI')
         logger.debug('Passed in values (if any):')
-        logger.debug('  Hue IP:    ' + hueIP)
-        logger.debug('  Hue Light: ' + hueLight)
+        logger.debug('  Hue IP:    ' + hue_IP)
+        logger.debug('  Hue Light: ' + hue_light)
 
         # Define the window's contents
         layout = [[sg.Text('Philips Hue Bridge Configuration')],
-                  [sg.Text('Hue Bridge IP:'), sg.Input(default_text=hueIP, key='-HUE_BRIDGE-', enable_events=True)],
-                  [sg.Text('Hue Light(s): '), sg.Input(default_text=hueLight, key='-HUE_LIGHT-', pad=((10,0),(10,0)))],
-                  [sg.Button('Ok', bind_return_key=True), sg.Button('Cancel')]
-        ]
+                  [sg.Text('Hue Bridge IP:'), sg.Input(default_text=hue_IP, key='-HUE_BRIDGE-', enable_events=True)],
+                  [sg.Text('Hue Light(s): '),
+                   sg.Input(default_text=hue_light, key='-HUE_LIGHT-', pad=((10, 0), (10, 10)))],
+                  [sg.Button('Ok', bind_return_key=True), sg.Button('Scan for Bridge'), sg.Button('Cancel')]]
         # Create the window
         window = sg.Window('Configure Hue Bridge', layout)
 
@@ -51,8 +51,8 @@ def GUImain():
                 try:
                     ipaddress.ip_address(values['-HUE_BRIDGE-'])
                     logger.debug('Valid IP address given: ' + values['-HUE_BRIDGE-'])
-                    hueIP = values['-HUE_BRIDGE-']
-                    hueLight = values['-HUE_LIGHT-']
+                    hue_IP = values['-HUE_BRIDGE-']
+                    hue_light = values['-HUE_LIGHT-']
                     break
                 except ValueError:
                     logger.debug('Invalid IP address entered.  Showing error window.')
@@ -64,13 +64,12 @@ def GUImain():
 
         # Finish up by removing from the screen
         logger.debug('Closing Configure window and returning:')
-        logger.debug('  Hue IP:    ' + hueIP)
-        logger.debug('  Hue Light: ' + hueLight)
+        logger.debug('  Hue IP:    ' + hue_IP)
+        logger.debug('  Hue Light: ' + hue_light)
         window.close()
-        return hueIP, hueLight
+        return hue_IP, hue_light
 
-
-    hueIP, hueLight, debug = initialize()
+    hue_IP, hueLight, debug = initialize()
     configure_logger()
     # create logger with 'EDHue'
     logger = logging.getLogger('EDHue.GUI')
@@ -78,13 +77,13 @@ def GUImain():
     logger.info('Starting.')
 
     # Define my theme
-    sg.LOOK_AND_FEEL_TABLE['EliteTheme'] = {'BACKGROUND': '#000000',
-                                            'TEXT': '#ffffff',
-                                            'INPUT': '#f07b05',
-                                            'TEXT_INPUT': '#ffffff',
-                                            'SCROLL': '#f07b05',
-                                            'BUTTON': ('white', '#f07b05'),
-                                            'PROGRESS': ('#01826B', '#D0D0D0'),
+    sg.LOOK_AND_FEEL_TABLE['EliteTheme'] = {'BACKGROUND': '#000000',    # Black
+                                            'TEXT': '#ffffff',          # White
+                                            'INPUT': '#f07b05',         # Elite Orange
+                                            'TEXT_INPUT': '#ffffff',    # White
+                                            'SCROLL': '#f07b05',        # Elite Orange
+                                            'BUTTON': ('white', '#f07b05'), # White on Elite Orange
+                                            'PROGRESS': ('#ffffff', '#000000'), # White on Black
                                             'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,
                                             }
 
@@ -94,14 +93,19 @@ def GUImain():
     # sg.popup_get_text('This how the MyNewTheme custom theme looks')
 
     # Define the window's contents
-    layout = [[sg.Button(key='StartImage', image_filename=os.path.join('assets', 'edlogo2.gif'), pad=(0,0)),
-               sg.Button(button_text='Elite Dangerous Hue Light Sync', key='StartText', font=('euro caps', 38), pad=(0,0))],
-              [sg.Text('Configured Bridge:', justification='right', size=(18,0)), sg.Text(hueIP, key='Bridge', justification='left', size=(18,0))],
-              [sg.Text('Configured Light:', justification='right', size=(18,0)), sg.Text(hueLight, key='Light', justification='left', size=(18,0))],
-              [sg.Button('Configure', font=('euro caps', 32), pad=((0,6),(0,0))), sg.Button('Exit', size=(4,1), pad=((545,0),(0,0)), font=('euro caps', 32))]
-    ]
+    layout = [[sg.Button(key='StartImage', image_filename=os.path.join('assets', 'edlogo2.gif'), pad=(0, 0)),
+               sg.Button(button_text='Elite Dangerous Hue Light Sync', key='StartText', font=('euro caps', 38),
+                         pad=(0, 0))],
+              [sg.Text('Configured Bridge:', justification='right', size=(18, 0)),
+               sg.Text(hue_IP, key='Bridge', justification='left', size=(18, 0))],
+              [sg.Text('Configured Light:', justification='right', size=(18, 0)),
+               sg.Text(hueLight, key='Light', justification='left', size=(18, 0))],
+              [sg.Button('Configure', font=('euro caps', 32), pad=((0, 6), (0, 0))),
+               sg.Button('Exit', size=(4, 1), pad=((545, 0), (0, 0)), font=('euro caps', 32))]
+              ]
     # Create the window
-    window = sg.Window('E:D Hue Light Sync', layout, margins=(25,25), background_color='#000000', element_justification='c')
+    window = sg.Window('E:D Hue Light Sync', layout, margins=(25, 25), background_color='#000000',
+                       element_justification='c')
 
     # Display and interact with the Window using an Event Loop
     while True:
@@ -113,16 +117,16 @@ def GUImain():
             start(window.AllKeysDict['Light'].DisplayText)
         if event == 'Configure':
             window.hide()
-            bridgeIP, bridgeLight = configure_ui(hueIP, hueLight)
+            bridgeIP, bridgeLight = configure_ui(hue_IP, hueLight)
             window.un_hide()
             logger.debug('Returned bridge IP: ' + bridgeIP)
             logger.debug('Returned Light: ' + bridgeLight)
             window['Bridge'].update(bridgeIP)
             window['Light'].update(bridgeLight)
-    # Finish up by removing from the screen
+    # Finish up by removing from the screen and doing some cleanup
     window.close()
-    sg.popup_auto_close('See you in the Black, Commander.\n\no7', background_color='#000000', text_color='#f07b05', no_titlebar=True, button_type=5, font=('euro caps', 32))
-
+    sg.popup_auto_close('See you in the Black, Commander.\n\no7', background_color='#000000', text_color='#f07b05',
+                        no_titlebar=True, button_type=5, font=('euro caps', 32))
     cleanup(window.AllKeysDict['Light'].DisplayText)
 
 
